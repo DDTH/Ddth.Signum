9ef8581ee90fc2aa7e09c0dc446589124bbc97d7
Thanh Nguyen <btnguyen2k@gmail.com>
2026-06-16 15:23:59 +1000

    Add XxHash3Hasher and Crc32Hasher pre-implemented hashers

---
+++ Ddth.Signum/XxHash3Hasher.cs
@@ -0,0 +1,21 @@
+using System.IO.Hashing;
+
+namespace Ddth.Signum;
+
+/// <summary>
+/// <see cref="IHasher"/> implementation backed by the fast, non-cryptographic
+/// <see cref="XxHash3"/> algorithm (64-bit output) from the <c>System.IO.Hashing</c> package.
+/// </summary>
+public sealed class XxHash3Hasher : IHasher
+{
+    private readonly XxHash3 _inner = new();
+
+    /// <inheritdoc/>
+    public int HashLengthInBytes => _inner.HashLengthInBytes;
+
+    /// <inheritdoc/>
+    public void Append(ReadOnlySpan<byte> data) => _inner.Append(data);
+
+    /// <inheritdoc/>
+    public void GetHashAndReset(Span<byte> destination) => _inner.GetHashAndReset(destination);
+}
